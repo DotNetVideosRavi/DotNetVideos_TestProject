4b71e09c88b7fa2743f034e928a99e2a6882ebf6
Ravi N <Ravi@Win2012RC.(none)>
2012-07-07 07:30:09 -0700

    Added sample app, first commit

---
+++ Getting Started with ASP.NET 4.5 Web Forms/C#/WingtipToys/Site.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = db.Categories;
+            return query;
+        }
+    }
+}
